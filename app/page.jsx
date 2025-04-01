da08c2fbbbb5369a91f23d8e1b66aa015ffcfb63
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-04-01 21:10:32 +0530

    sidebar component added

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
 import { useState } from "react";
+import Sidebar from '@/components/sidebar'
 
 export default function Home() {
   const [expand, setExpand] = useState(false);
@@ -11,7 +12,10 @@ export default function Home() {
   return (
     <div>
       <div className='flex h-screen'>
+
         {/* -- sidebar -- */}
+        <Sidebar expand={expand} setExpand={setExpand} />
+
         <div className='flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative' >
           <div className='md:hidden absolute px-4 top-6 flex items-center justify-between w-full'>
             <Image onClick={()=> (expand ? setExpand(false) : setExpand(true))} className='rotate-180' src={assets.menu_icon} alt='image' />
@@ -32,6 +36,8 @@ export default function Home() {
           <div></div>
         )
         }
+        {/* prompt box */}
+        <p className='text-xs absolute bottom-1 text-gray-500'>AI-generated, for reference only</p>
         </div>
       </div>
     </div>
