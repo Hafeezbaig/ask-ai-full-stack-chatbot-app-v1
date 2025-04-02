5ced285f82058a5522eb44139e786b4337664c5d
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-04-02 18:21:17 +0530

    sidebar updated and icons added

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const sidebar = ({ expand, setExpand }) => {
     return (
-        <div className={`flex flex-col justify-between bg=[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'}`}>
+        <div className={`flex flex-col justify-between bg-[#1b1c1e] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'}`}>
             <div>
                 <div className={`flex ${expand ? 'flex-row gap-10' : 'flex-col items-center gap-8'}`}>
                     <Image className={expand ? 'w-36' : 'w-10'} src={expand ? assets.logo_text : assets.logo_icon} alt='image' />
@@ -12,7 +12,7 @@ const sidebar = ({ expand, setExpand }) => {
                         <Image src={assets.menu_icon} alt='image' className='md:hidden' />
                         <Image src={expand ? assets.sidebar_close_icon : assets.sidebar_icon} alt='image' className='hidden md:block w-7 ' />
 
-                        <div>
+                        <div className={`absolute w-max ${expand ? 'left-1/2 -translate-x-1/2 top-12' : '-top-12 left-0'} opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none `}>
                             {expand ? 'Close Sidebar' : 'Open Sidebar'}
                             <div className={`w-3 h-3 absolute bg-black rotate-45 ${expand ? 'left-1/2 -top-1.5 -translate-x-1/2' : 'left-4 -bottom-1.5'}`}></div>
                         </div>
@@ -20,6 +20,17 @@ const sidebar = ({ expand, setExpand }) => {
 
                     </div>
                 </div>
+
+                <button className={`mt-8 flex items-center justify-center cursor-pointer ${expand ? 'bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5 w-max' : 'group relative h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg'}`}>
+                    <Image className={expand ? 'w-6' : 'w-7'} src={expand ? assets.chat_icon : assets.chat_icon_dull} alt='image' />
+                    <div className='absolute w-max -top-12 -right-12 opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none'>
+                        New Chat
+                        <div className='w-3 h-3 absolute bg-black rotate-45 left-4 -bottom-1.5'></div>
+                    </div>
+                    {expand && <p className='text-white text font-medium'>New Chat</p>}
+                </button>
+
+
             </div>
         </div>
     )
