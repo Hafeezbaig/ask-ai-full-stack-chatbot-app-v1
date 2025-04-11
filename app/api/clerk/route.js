9989e62572a939af6529a7c193abee95e68109c8
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-04-12 01:53:08 +0530

    v9 - bugs fixed

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -10,7 +10,7 @@ export async function POST(req) {
     const svixHeaders = {
         "svix-id": headerPayload.get("svix-id"),
         "svix-signature": headerPayload.get("svix-signature"),
-        // "svix-timestamp": headerPayload.get("svix-timestamp"),
+        "svix-timestamp": headerPayload.get("svix-timestamp"),
     };
 
     // get the payload and verify it
